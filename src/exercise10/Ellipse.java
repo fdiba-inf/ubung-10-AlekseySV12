a9d623a123dae6d93fafc3cd90b4276196fde3c5
AlekseySV12 <72090550+AlekseySV12@users.noreply.github.com>
2020-12-02 12:04:46 +0200

    Update Ellipse.java
    .

--- src/exercise10/Ellipse.java
+++ src/exercise10/Ellipse.java
@@ -49,8 +49,8 @@ public class Ellipse extends Figure{
         double centerX = startPoint.getX() + side1;
         double centerY = startPoint.getY() + side2;
 
-        double part1 = Math.pow(clickX - centerX,2)/side1*side1;
-        double part2 = Math.pow(clickY - centerY,2)/side2*side2;
+        double part1 = Math.pow(clickX - centerX,2)/(side1*side1);
+        double part2 = Math.pow(clickY - centerY,2)/(side2*side2);
         return part1 + part2 <=1;
     }
 }
